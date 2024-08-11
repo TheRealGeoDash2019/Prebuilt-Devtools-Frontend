b6630b8ad7e41396972bc22cfb8556c332557e30
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-08-11 16:14:22 -0400

    Create Theme.js

---
+++ extra/Theme.js
@@ -0,0 +1,11 @@
+(() => {
+    const _ = new URL(location.href);
+    const accentParam = _.searchParams.get("accent");
+    if (accentParam) {
+        const __ = document.createElement(`link`);
+        __.rel = "stylesheet";
+        __.href = "/extra/Theme.css";
+        document.head.appendChild(__);
+        document.querySelector("html").style += ("--palette-primary: " + accentParam + "deg;");
+    }
+})();
