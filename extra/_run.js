d8524958067e769b5d75557b83d08c29c43a8c30
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-08-13 18:52:03 -0400

    Update _run.js

--- extra/_run.js
+++ extra/_run.js
@@ -22,7 +22,7 @@ console.log(`[Patcher] Patched: devtools_app.html, index.html`);
 const msgConnContents = fs.readFileSync(MESSAGE_CONNECTION_TEMPLATE, "utf-8");
 const connectionContents = fs.readFileSync(CONNECTIONS_FILE, "utf-8");
 const newConnContents = connectionContents.replace(/export class WebSocket/gmi, function(old) {
-    return (msgConnContents + "\n" + old);
+    return (msgConnContents + old);
 });
 fs.writeFileSync(CONNECTIONS_FILE, newConnContents);
 console.log(`[Patcher] Patched: /core/sdk/Connections.js`);
