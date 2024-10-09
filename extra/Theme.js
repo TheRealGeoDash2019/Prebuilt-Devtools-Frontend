ca5d4616e4a90c40a90f2c94c69ae1fbb085f8da
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-10-09 11:40:11 -0400

    Update Theme.js

--- extra/Theme.js
+++ extra/Theme.js
@@ -1,11 +1,15 @@
 (() => {
     const _ = new URL(location.href);
     const accentParam = _.searchParams.get("accent");
-    if (accentParam) {
+    const rainbowParam = _.searchParams.has("rainbow");
+    if (accentParam || rainbowParam) {
         const __ = document.createElement(`link`);
         __.rel = "stylesheet";
         __.href = "/extra/Theme.css";
         document.head.appendChild(__);
         document.querySelector(`html`).style.setProperty(`--palette-primary`, `${accentParam}deg`);
+        if (rainbowParam) {
+          document.querySelector("html").style.animation = "3s linear rainbowUi infinite";
+        }
     }
 })();
