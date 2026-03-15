31addc39498abf13b66302729adc93f412572841
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2026-03-15 01:53:04 -0400

    Update logging and disable panels in _prerun.js

--- extra/_prerun.js
+++ extra/_prerun.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 
-console.log("Starting Patcher")
+console.log("[Patcher] Starting Patcher")
 
 const DEVTOOLS_FRONTEND = process.cwd();
 const FRONTEND_DIR = path.join(DEVTOOLS_FRONTEND, "front_end");
@@ -10,12 +10,17 @@ const PANELS_DIR = path.join(FRONTEND_DIR, "panels");
 const permittedPanels = ["application", "console", "developer_resources", "elements", "network", "protocol_monitor", "settings", "sources"];
 
 const PANELS = fs.readdirSync(PANELS_DIR, { withFileTypes: true }).filter(o => o.isDirectory());
+
+console.log(`[Patcher] Found ${PANELS.length} Panel Directories`);
+
 for (const panel of PANELS) {
   const HAS_META = fs.existsSync(path.join(PANELS_DIR, panel.name + "-meta.ts"));
   if (HAS_META && !permittedPanels.includes(panel.name)) {
+    console.log(`[Patcher] Disabling ${panel.name} from DevTools`);
     // const META_CONTENTS = fs.readFileSync(path.join(PANELS_DIR, panel.name + "-meta.ts"));
     // const NEW_CONTENTS = META_CONTENTS.replaceAll("UI.ViewManager.registerViewExtension", ";(function noop(){})");
-    fs.writeFileSync(path.join(PANELS_DIR, panel.name + "-meta.ts"), "// @ts-nocheck");
+    fs.writeFileSync(path.join(PANELS_DIR, panel.name + "-meta.ts"), Buffer.from([]));
+    console.log(`[Patcher] Disabled ${panel.name}`);
   }
 }
 
